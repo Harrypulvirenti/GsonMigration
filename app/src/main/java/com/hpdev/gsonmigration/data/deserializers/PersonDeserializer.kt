af2c1aeb7b1b1a45d022c3782902a85f80e5f5a8
harrypulvirenti <harrypulvirentidev@gmail.com>
2022-01-09 16:36:23 +0100

    Implement custom deserializers

---
+++ app/src/main/java/com/hpdev/gsonmigration/data/deserializers/PersonDeserializer.kt
@@ -0,0 +1,30 @@
+package com.hpdev.gsonmigration.data.deserializers
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.hpdev.gsonmigration.data.dto.AddressDTO
+import com.hpdev.gsonmigration.data.dto.PersonDTO
+import java.lang.reflect.Type
+
+class PersonDeserializer : JsonDeserializer<PersonDTO> {
+
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): PersonDTO = with(json.asJsonObject) {
+        PersonDTO(
+            id = getNotNull("id"),
+            name = getNotNull("name"),
+            surname = getNotNull("surname"),
+            address = deserializeAddressDTO(context)
+        )
+    }
+
+    private fun JsonObject.deserializeAddressDTO(context: JsonDeserializationContext): AddressDTO? =
+        context.deserialize(getOrNull<JsonObject>("address"), getType<AddressDTO>())
+}
+
+
