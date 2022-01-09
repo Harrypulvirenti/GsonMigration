2a3973f104c1e9e412064cf5e4fb9a60e147edd3
harrypulvirenti <harrypulvirentidev@gmail.com>
2022-01-09 16:33:33 +0100

    Update fields nullability

--- app/src/main/java/com/hpdev/gsonmigration/data/dto/DTOModels.kt
+++ app/src/main/java/com/hpdev/gsonmigration/data/dto/DTOModels.kt
@@ -2,15 +2,15 @@ package com.hpdev.gsonmigration.data.dto
 
 // Gson Objects
 data class PersonDTO(
-    val id: String?,
-    val name: String?,
-    val surname: String?,
+    val id: String,
+    val name: String,
+    val surname: String,
     val address: AddressDTO?
 )
 
 data class AddressDTO(
-    val street: String?,
-    val number: Int?,
+    val street: String,
+    val number: Int,
 )
 
 
